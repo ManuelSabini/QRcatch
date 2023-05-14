97d68e18b9229f833ab370f71c60b100de3e930c
Manuel Sabini <manusabi13@gmail.com>
2023-05-14 00:26:36 -0300

    Se finalizo con el archivo de JS y se añadio manejo de errores por consola, lista para el deploy

--- js/site.js
+++ js/site.js
@@ -1,6 +1,6 @@
 function iniciarListeners(){
     /* Boton Generar */
-    botonGenerar = document.querySelector("#boton_generar");
+    botonGenerar = document.querySelector("#button-addon2");
     botonGenerar.addEventListener("click", (e)=> {
         console.log("Se preciono el boton Generar");
         capturaDeDatos()
@@ -14,25 +14,36 @@ function capturaDeDatos(){
     formato = document.querySelector("#selectorFormato").value;
     /* Captura del valor seleccionado */
     valor = document.querySelector("#valor").value;
-    insercionCodigo(valor, formato);
-    tamaño(tamaño);
-}
-
-function tamaño(){
-    /* Modifica una class del body para ajustar el tamaño */
-}
-
-function insercionCodigo(){
-    /* Revisa el Main, si existe lo borra y luego lo genera */
-    main = document.querySelector(".codigoBarrasoQr")
-    if (main != null) {
-        /* Borrar el DIV con class codigoBarrasoQr*/
+    if (valor) {
+        tamañoBody(tamaño);
+        insercionCodigo(valor, formato);
+    }else{
+        document.querySelector(".codigoBarrasoQr").remove();
+        console.log("El campo valor esta vacio, no se pudo generar el QR")
     }
-    /* Insertar el DIV */
-    /* Crear el div */
-    /* Asignarle las clases */
-    /* Inner HTML */
-    /* Append child al Main */
+}
+
+function tamañoBody(tamaño){
+    /* Modifica una class del body para ajustar el tamaño */
+    body = document.querySelector("body").classList;
+    body.remove("tamaño-grande");
+    body.remove("tamaño-mediano");
+    body.remove("tamaño-pequeño");
+    body.add(tamaño);
+}
+
+function insercionCodigo(valor, formato){
+    /* Revisa el Main, si existe lo borra y luego lo genera */
+    imgCod = document.querySelector(".codigoBarrasoQr")
+    if (imgCod != null) {
+        document.querySelector(".codigoBarrasoQr").remove();
+    }
+    /* Insertar el IMG dentro del div, tambien le asigno clases y lo meto en el Main*/
+    imgCod = document.createElement("div");
+    imgCod.classList.add("col-10", "codigoBarrasoQr");
+    imgCod.innerHTML = `<img src="https://barcode.tec-it.com/barcode.ashx?data=${valor}&code=${formato}&translate-esc=true&dpi=300&imagetype=Png" alt="barcode/QRcode" id="codigo">`;
+    document.querySelector("main").appendChild(imgCod);
+    console.log(`Se genero el codigo. Formato: ${formato}. Valores Generados: ${valor}`)
 }
 
 
@@ -41,5 +52,8 @@ let botonGenerar;
 let tamaño;
 let formato;
 let valor;
-let main;
+let imgCod;
+let body;
+let errorMsj;
 
+iniciarListeners();
